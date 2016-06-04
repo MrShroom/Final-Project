325f4a906ea07729f6005c5c2e616dc63b4e92b6
Matthew Yefima <matt.yefima@gmail.com>
2016-06-03 22:37:53 -0700

    Added Scripts
    Scripts for:
    Octree and Octree Nodes
    Brute force collision detection
    Individual collision detection for most interactions

---
+++ Octree Scripts/CollisionCheck.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (Transform t in transform) {
+			GameObject o = t.gameObject;
+			if(o.GetComponent<OurCollider>()){
+				foreach(Transform r in transform){
+					GameObject g = r.gameObject;
+					if(g ==  o) continue;
+					else if(o.GetComponent<OurCollider>().doesCollide (g))
+						print ("Collision");
+				}
+			}
+		}
+	}
+}

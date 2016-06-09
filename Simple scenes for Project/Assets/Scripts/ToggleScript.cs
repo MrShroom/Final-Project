e16116c6f10db1bba22c4fc969f92e31d14ed4f0
Matthew Yefima <matt.yefima@gmail.com>
2016-06-09 13:23:50 -0700

    Added spacebar to toggle script

---
+++ Simple scenes for Project/Assets/Scripts/ToggleScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleScript : MonoBehaviour {
+
+	public GameObject collisionChecker;
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space))
+			collisionChecker.GetComponent<CollisionCheck> ().OctreeToggle ();
+	}
+}

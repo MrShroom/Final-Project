4c484c2248e2b8cb1656504da2d375ab4ef6b494
Matthew Yefima <matt.yefima@gmail.com>
2016-06-09 00:17:08 -0700

    Collision Detection Fixes
    Cube-Cube and Cube-Sphere intersection tests corrected

--- Simple scenes for Project/Assets/Scripts/OurCollider.cs
+++ Simple scenes for Project/Assets/Scripts/OurCollider.cs
@@ -6,26 +6,38 @@ public class OurCollider : MonoBehaviour {
 	bool SphereCollision(GameObject o){
 		if (o.tag == "Sphere") {
 			Vector3 distanceBetweenCenters = transform.position - o.transform.position;
-			float sumOfRadii = GetComponent<SphereCollider>().radius * transform.lossyScale.x + o.GetComponent<SphereCollider>().radius * o.transform.lossyScale.x;
+			float sumOfRadii = GetComponent<SphereCollider>().radius + o.GetComponent<SphereCollider>().radius;
 			if(distanceBetweenCenters.magnitude <= sumOfRadii)
 				return true;
 		}
 		if (o.tag == "Plane") {
 			Plane p = new Plane(new Vector3(0,1,0), new Vector3(0,0,0));
 			float distance = p.GetDistanceToPoint(transform.position);
-			if(distance <= GetComponent<SphereCollider>().radius * transform.lossyScale.x)
+			if(distance <= GetComponent<SphereCollider>().radius)
 				return true;
 		}
 		if (o.tag == "Box") {
-			float r2 = GetComponent<SphereCollider>().radius * transform.lossyScale.x * GetComponent<SphereCollider>().radius * transform.lossyScale.x;
-			float dmin = 0;
-			for( int i = 0; i < 3; ++i) {
-				if(transform.position[i] <= o.GetComponent<BoxCollider>().bounds.min[i])
-					dmin += Mathf.Sqrt (transform.position[i] - o.GetComponent<BoxCollider>().bounds.min[i] );
-				else if (transform.position[i] >= o.GetComponent<BoxCollider>().bounds.max[i])
-					dmin += Mathf.Sqrt (transform.position[i] - o.GetComponent<BoxCollider>().bounds.max[i]);
-			}
-			return dmin <= r2;
+
+			Bounds b = o.GetComponent<BoxCollider>().bounds;
+
+
+			float xDist = Mathf.Abs(transform.position.x - o.transform.position.x);
+			float yDist = Mathf.Abs(transform.position.y - o.transform.position.y);
+			float zDist = Mathf.Abs(transform.position.z - o.transform.position.z);
+
+			if(xDist >= (b.size.x + GetComponent<SphereCollider>().radius)) return false;
+			if(yDist >= (b.size.y + GetComponent<SphereCollider>().radius)) return false;
+			if(zDist >= (b.size.z + GetComponent<SphereCollider>().radius)) return false;
+
+			if(xDist < b.size.x) return true;
+			if(yDist < b.size.y) return true;
+			if(zDist < b.size.z) return true;
+
+			float squaredDistance = Mathf.Pow ((xDist - b.size.x), 2) + 
+									Mathf.Pow ((yDist - b.size.y), 2) + 
+									Mathf.Pow ((zDist - b.size.z), 2); 
+
+			return squaredDistance < Mathf.Pow (GetComponent<SphereCollider>().radius, 2);
 		}
 		
 		return false;
@@ -39,19 +51,16 @@ public class OurCollider : MonoBehaviour {
 			return o.GetComponent<OurCollider>().PlaneCollision(this.gameObject);
 		}
 		if (o.tag == "Box") {
-			Plane p = new Plane(new Vector3(0,1,0), new Vector3(0,0,0));
-			Vector3 cornerA = o.transform.position;
-			Vector3 cornerB = o.transform.position;
-			cornerA += o.GetComponent<BoxCollider>().size / 2;
-			cornerB -= o.GetComponent<BoxCollider>().size / 2;
-			
-			if( Vector3.Dot(p.normal,cornerA) > 0 && Vector3.Dot (p.normal,cornerB) > 0 )
-				return false;
-			else if( Vector3.Dot(p.normal,cornerA) < 0 && Vector3.Dot (p.normal,cornerB) < 0 )
-				return false;
-			else
-				return true;
-			
+
+			Vector3 thisMin = GetComponent<BoxCollider>().bounds.min;
+			Vector3 oMin = o.GetComponent<BoxCollider>().bounds.min;
+
+			Vector3 thisMax = GetComponent<BoxCollider>().bounds.max;
+			Vector3 oMax = o.GetComponent<BoxCollider>().bounds.max;
+
+			return (thisMax.x >= oMin.x && thisMin.x <= oMax.x)
+					&& (thisMax.y >= oMin.y && thisMin.y <= oMax.y)
+					&& (thisMax.z >= oMin.z && thisMin.z <= oMax.z);
 		}
 		return false;	
 	}
@@ -60,7 +69,7 @@ public class OurCollider : MonoBehaviour {
 		Plane p = new Plane(new Vector3(0,1,0), new Vector3(0,0,0));
 		if (o.tag == "Sphere") {
 			float distance = p.GetDistanceToPoint(o.transform.position);
-			if(distance <= o.GetComponent<SphereCollider>().radius * o.transform.lossyScale.x)
+			if(distance <= o.GetComponent<SphereCollider>().radius)
 				return true;
 		}
 		if (o.tag == "Plane") {
